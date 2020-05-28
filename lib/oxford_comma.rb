7e8beb60df71a25505c46ce345accbd9cc76d146
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-05-28 20:03:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,7 @@ def oxford_comma(array)
     array [0] + " and " + array[1]
   else
     #binding.pry 
-    array[-1] = "and " array[-1]
+    array[-1] = "and " + array[-1]
+    array.join(",")
   end
 end
